42a583552ccd53ef17b827fe8c9623a20a5e4d00
Van Celdran <celdranvan@gmail.com>
2023-05-21 19:29:38 -0700

    add highlight CRUD and routes

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,2 +1,32 @@
 class HighlightsController < ApplicationController
+
+    wrap_parameters format:[]
+
+    def index
+        highlights = Highlight.all
+        render json: highlights, status: :ok
+    end
+
+    def show
+        
+    end
+
+    def create
+        highlight = Highlight.create(highlight_params)
+        render json: highlight, status: :created
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
+    private
+
+    def highlight_params
+        params.permit(:title, :description, :video_url, :user_id, :game_id)
+    end
 end
