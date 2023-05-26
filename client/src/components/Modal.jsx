9823ee9564ec3191d6e3f87b1b90caf2b468cb23
Van Celdran <celdranvan@gmail.com>
2023-05-25 19:43:17 -0700

    optimize app

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -3,59 +3,54 @@ import "../styles/Modal.css";
 import ReactModal from "react-modal";
 import { toast } from "react-toastify";
 
-const Modal = ({ isOpen, closeModal, selectedHighlight, currentUser }) => 
-{
+const Modal = ({ isOpen, closeModal, selectedHighlight, currentUser }) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [comments, setComments] = useState([])
-
-    console.log(selectedHighlight)
-    useEffect(() => {
-        fetch(`/highlights/${selectedHighlight.id}/comments`)
-        .then(r => r.json())
-        .then(commentsData => setComments(commentsData))
-    },[])
-  const commentsData = comments.map((comment) => {
-    return (
-      <div className="comment" key={comment.id}>
-        <div className="comment-profile">
-        {comment.user.image_url&&<img src={comment.user.image_url} alt="" />}
-        <p>{comment.user.name.toUpperCase()}</p>
-        </div>
-        
-        <p id="content">{comment.content}</p>
-      </div>
-    );
-  });
-
-  const [content, setContent] = useState("")
-  console.log(content)
-
-  function handleSubmit(e){
-    e.preventDefault()
-    fetch(`/highlights/${selectedHighlight.id}/comments`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ 
-            content: content,
-            user_id: selectedHighlight.user_id,
-            highlight_id: selectedHighlight.id
-
-
-        }),
-      }).then((r) => {
-        if (r.ok) {
-          
-          r.json().then((newComment) => console.log(newComment));
-    
-         
-        } else {
-          r.json().then((err) => toast.error(err.errors[0]));
-        }
+  useEffect(() => {
+    fetch(`/highlights/${selectedHighlight.id}/comments`)
+      .then((r) => r.json())
+      .then((commentsData) => {
+        setComments(commentsData);
+        setIsLoading(false); // Set loading state to false when comments are fetched
       });
+  }, []);
+
+  const commentsData = comments.map((comment) => (
+    
+    <div className="comment" key={comment.id}>
+        
+      <div className="comment-profile">
+        {comment.user.image_url && <img src={comment.user.image_url} alt="" />}
+        <p>{comment.user.name.toUpperCase()}</p>
+      </div>
+      <p id="content">{comment.content}</p>
+    </div>
+  ));
+
+  const [content, setContent] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`/highlights/${selectedHighlight.id}/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: content,
+        user_id: selectedHighlight.user_id,
+        highlight_id: selectedHighlight.id,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((newComment) => console.log(newComment));
+      } else {
+        r.json().then((err) => toast.error(err.errors[0]));
+      }
+    });
   }
-  
+
   return (
     <ReactModal
       isOpen={isOpen}
@@ -64,31 +59,35 @@ const Modal = ({ isOpen, closeModal, selectedHighlight, currentUser }) =>
       onRequestClose={closeModal}
     >
       <div className="modal-content">
-       
         <div className="profile">
           {selectedHighlight.user.image_url && (
             <img src={selectedHighlight.user.image_url} alt="" />
           )}
           <h3>{selectedHighlight.user.name.toUpperCase()}</h3>
         </div>
-     
         <h2>Title: {selectedHighlight.title}</h2>
-      
-
         <p>Description: {selectedHighlight.description}</p>
-        {selectedHighlight.comments.length <= 0 ? (
-          <h1>No comments yet!</h1>
+        {isLoading ? (
+          <h1>Loading comments...</h1>
         ) : (
-          ""
+          <>
+            {selectedHighlight.comments.length <= 0 ? (
+              <h1>No comments yet!</h1>
+            ) : (
+              ""
+            )}
+            {commentsData}
+          </>
         )}
-        {commentsData}
-       
-        
       </div>
       <form onSubmit={handleSubmit}>
-          {currentUser.image_url &&<img src={currentUser.image_url} alt="" />}
-          <input type="text" placeholder="Write your comment..." onChange={(e) => setContent(e.target.value)}/>
-        </form>
+        {currentUser.image_url && <img src={currentUser.image_url} alt="" />}
+        <input
+          type="text"
+          placeholder="Write your comment..."
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </form>
     </ReactModal>
   );
 };
