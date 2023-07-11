73a78eac622615ae5ca201159f2595843a1c5680
Van Celdran <celdranvan@gmail.com>
2023-07-11 14:08:26 -0700

    update directory again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   
 
   def fallback_index_html
-    render file: "client/public/index.html"
+    render file: "../client/public/index.html"
   end
 
   private
