0662503b0f6ee6183fc811c364256d95b9416694
Van Celdran <celdranvan@gmail.com>
2023-07-11 13:33:53 -0700

    try 10

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,6 @@ Rails.application.routes.draw do
   resources :games, only: [:index]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", via: :all
 end
